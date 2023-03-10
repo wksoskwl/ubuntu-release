1722f66841002f4f14abd304db5a51112527e08c
wksoskwl <kms20060827@gmail.com>
2023-03-09 19:35:38 -0800

    file upload

---
+++ file.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+	FILE *file;
+	
+	file = fopen("test.txt","r");
+
+	printf("%c\n",fgetc(file));
+
+	fclose(file);
+}
---
+++ file2.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(){
+    int fd;
+    char pathname[] = "temp.txt";
+
+    if((fd=open(pathname, O_CREAT | O_RDWR, 0644)) == -1)
+    {
+        printf("ERROR!!!\n");
+        exit(1);
+    }
+    close(fd);
+}
+
