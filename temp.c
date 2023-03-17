41353573a6448e8e5cc22535cb942e771b82a50e
wksoskwl <kms20060827@gmail.com>
2023-03-16 20:10:38 -0700

    commit

--- temp.c
+++ temp.c
@@ -1,21 +1,35 @@
-#include <stdlib.h>
 #include <stdio.h>
-  #include <sys/types.h>
-  #include <fcntl.h>
-  #include <unistd.h>
-  #include <string.h>
-  #include <errno.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
 
-int main(){
-    int fd = open("temp0.txt", O_RDWR|O_CREAT, 0644);
-    char a[1024] = "a1bcabcAZ23";
-	write(fd, a, 1024);
-	int cnt = 0;
-	for(int i = 0;a[i] != '\0';i++){
-	if(a[i] >= 'A' && a[i] <= 'Z' || a[i] >= 'a' && a[i] <= 'z')	cnt++;
-}
-printf("%d\n",cnt);
-    close(fd);
+void readFile(){
+	int fd = open("temp.txt", O_RDWR | O_CREAT, 0664);
+	char a[1024] = "yoochanhong";
+	write(fd, a, 12);
+	close(fd);
 }
 
+int calculator(){
+	int count = 0;
+	int fd = open("temp.txt", O_RDWR | O_CREAT, 0664);
+	char b[1024] = {};
+	read(fd, b, 12);
+	for (int i = 0; i < strlen(b); i++){
+		if((b[i] >= 65 && b[i] <= 90) || (b[i] >= 97 && b[i] <= 122)) count++;
+	}
+	close(fd);
+	printf("%s(%d)\n", strerror(errno), errno);
+	return count;
+}
 
+
+int main()
+{
+	readFile();
+	int c = calculator();
+	printf("%d\n", c);
+}
