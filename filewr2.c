56246013d87fd7fb44d17658ee639e208fa50795
wksoskwl <kms20060827@gmail.com>
2023-03-16 19:02:51 -0700

    commit

---
+++ filewr2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+
+void file1(char *);
+
+int main(int argc, char **argv){
+	if (argc != 2) exit(0);
+
+	file1(argv[1]);
+}
+
+void file1(char *filename){
+    int fd = open(filename, O_RDWR|O_CREAT, 0644);
+    char a[1024] = "01234567890123";
+    write(fd, a, 10);
+    close(fd);
+    
+    fd = open(filename, O_RDWR, 0644);
+    char b[1024] = {};
+    read(fd, b, 7);
+    close(fd);
+    printf("%s\n", b);
+}
