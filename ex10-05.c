4fa01c643d8d52ea5eb669d447ecc3747e3d31c8
wksoskwl <kms20060827@gmail.com>
2023-05-09 20:22:55 -0700

    2023-05-10

---
+++ ex10-05.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	sigset_t set;
+	int count = 5;
+
+	sigemptyset(&set);
+	sigfillset(&set);
+	
+	sigprocmask(SIG_BLOCK, &set, NULL);
+
+	while(count){
+		printf("don't disturb me (%d)\n",count--);
+		sleep(1);
+	}
+	sigemptyset(&set);
+	sigaddset(&set, SIGINT);
+	sigprocmask(SIG_UNBLOCK, &set, NULL);
+
+	printf("you did not disturb me!!\n");
+}
